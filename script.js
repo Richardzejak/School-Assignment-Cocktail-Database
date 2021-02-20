a8b9c50ddcb8e5ddfae477dbf5302b2683b5d46f
Calphonar <Richardzejak@hotmail.com>
2021-02-20 14:29:21 +0100

    Working toggle sidebar

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let sidebarToggle = document.getElementById("sidebar-toggle");
+let sidebar =  document.getElementById("sidebar");
+let contentColumn =  document.getElementById("contentColumn");
+let sidebarHidden = false;
+
+sidebarToggle.addEventListener("click", function(){
+
+if(sidebarHidden){
+sidebar.style.visibility="visible";
+contentColumn.style.paddingLeft="144px";
+sidebarHidden=false;
+}
+else if(sidebarHidden==false){
+sidebar.style.visibility="hidden";
+contentColumn.style.paddingLeft=0;
+sidebarHidden=true;
+}
+});
+
 async function changePicture() {
   fetch("http://www.omdbapi.com/?apikey=17ce5389&t:batman")
     .then((response) => response.json())
