7b397bbf55c77c3142a8164aac020a4f8497674e
Palmeuh <fredrik.palme@utb.ecutbildning.se>
2021-02-24 12:19:03 +0100

    created cards from fetch, also tried making a fade in animation

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let contentColumn = document.getElementById("contentColumn");
 let sidebarHidden = false;
 const cardContainer = document.getElementById("cardContainer");
 
+contentColumn.className = "box";
+
 sidebarToggle.addEventListener("click", function () {
   if (sidebarHidden) {
     sidebar.style.visibility = "visible";
@@ -16,65 +18,69 @@ sidebarToggle.addEventListener("click", function () {
   }
 });
 
-async function changePicture() {
-  fetch("http://www.omdbapi.com/?apikey=17ce5389&t:batman")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    });
-}
-
 /* Get data */
 async function getData() {
   fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
     .then((response) => response.json())
     .then((data) => {
-      let name = data.drinks[0].strDrink;
-      console.log(name);
+      console.log(data);
+
+      //create an array of the data objects as well as a new array for the drinks with only the properties we want
+      let drinkArray = data.drinks;
+      let filteredDrinks = [];
+
+      //create objects with the properties we want and then push them into new array
+      drinkArray.forEach((object) => {
+        let drink = {
+          name: object.strDrink,
+          photo: object.strDrinkThumb,
+        };
+        filteredDrinks.push(drink);
+      });
+      console.log(filteredDrinks);
+
+      //build the cards with the array of drink objects
+
+      buildCard(filteredDrinks);
     });
 }
 
 getData();
 
-//* Build cards */
+//* Build cards by sending in an array of the fetch response with drinks*/
 
-function buildGrid() {
-  for (let i = 0; i < 10; i++) {
-    buildCard();
-  }
+function buildCard(drinkArray) {
+  cardContainer.innerHTML = "";
+
+  drinkArray.forEach((drink) => {
+    let mainColumn = document.createElement("div");
+
+    mainColumn.className = "col-md-6 col-xl-4 mb-5";
+    cardContainer.appendChild(mainColumn);
+
+    let card = document.createElement("div");
+    card.className = "card h-100 border border-dark";
+    mainColumn.appendChild(card);
+
+    let image = document.createElement("img");
+    image.setAttribute("id", "myIcon");
+    image.setAttribute("src", drink.photo);
+    image.className = "card-img-top bg-dark";
+    card.appendChild(image);
+
+    let cBody = document.createElement("div");
+    cBody.className = "card-body bg-dark";
+    card.appendChild(cBody);
+
+    let title = document.createElement("h4");
+    title.className = "card-title text-light";
+    title.innerText = drink.name.toUpperCase();
+    cBody.appendChild(title);
+
+    let cardText = document.createElement("p");
+    cardText.className = "text-light";
+    cardText.innerHTML = "some more text";
+
+    cBody.appendChild(cardText);
+  });
 }
-
-function buildCard() {
-  let mainColumn = document.createElement("div");
-  mainColumn.className = "col-md-6 col-xl-4 mb-5";
-  cardContainer.appendChild(mainColumn);
-
-  let card = document.createElement("div");
-  card.className = "card h-100";
-  mainColumn.appendChild(card);
-
-  let image = document.createElement("img");
-  image.setAttribute("id", "myIcon");
-  image.setAttribute("src", "images/RB.JPG");
-  image.className = "card-img-top bg-dark";
-  card.appendChild(image);
-
-  let cBody = document.createElement("div");
-  cBody.className = "card-body";
-  card.appendChild(cBody);
-
-  let title = document.createElement("h4");
-  title.className = "card-title";
-  title.innerText = "Some title";
-  cBody.appendChild(title);
-
-  let cardText = document.createElement("p");
-  cardText.innerHTML = "some more text";
-  // "</br>" +
-  // "Adress: " +
-  // newAttraction.adress;
-
-  cBody.appendChild(cardText);
-}
-
-buildGrid();
